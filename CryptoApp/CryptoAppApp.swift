ce72ab3cdcc7d8817ed3966bac76c22c7ae21650
saqibbhatti <sqb_bhatti@yahoo.com>
2024-08-13 12:57:07 +0800

    Project Setup

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Saqib Bhatti on 13/8/24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
